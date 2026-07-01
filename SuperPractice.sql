4b870977df5c540b83bf645a0527ac4bf70885aa
TomBumSuChoi <qjatn0545@gmail.com>
2026-07-01 22:17:51 +0900

    -- Find the number of movies released each year.

--- SuperPractice.sql
+++ SuperPractice.sql
@@ -24,4 +24,26 @@ SELECT movies.rating,
 FROM movies
 WHERE rating > 6
 GROUP BY rating
-ORDER BY total_movies DESC
+ORDER BY total_movies DESC;
+
+-- Find the number of movies released each year.
+SELECT movies.release_date,
+       COUNT() AS total_movies
+FROM movies
+WHERE release_date IS NOT NULL
+GROUP BY release_date
+ORDER BY total_movies DESC;
+
+-- List the top 10 years with the highest average movie runtime.
+
+-- Calculate the average rating for movies released in the 21st century.
+-- Find the director with the highest average movie runtime.
+-- List the top 5 most prolific directors (those who have directed the most movies).
+-- Find the highest and lowest rating of each director.
+-- Find the director that has made the most money (revenue - budget)
+-- Calculate the average rating for movies longer than 2 hours.
+-- Find the year with the most movies released.
+-- Find the average runtime of movies for each decade.
+-- Calculate the percentage of movies with a rating above 8.0.
+-- List directors who have never made a movie shorter than 2 hours.
+-- Categorize and group movies by length.
