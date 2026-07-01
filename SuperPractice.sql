6177609e552023b00aeb43fa7cf3409ad9cbf2bd
TomBumSuChoi <qjatn0545@gmail.com>
2026-07-01 21:42:31 +0900

    장르별 영화 갯수 구하기

--- SuperPractice.sql
+++ SuperPractice.sql
@@ -1,9 +1,19 @@
 -- What is the average rating of each director?
+-- * that has more than 5 movies
 SELECT movies.director,
-       AVG(rating) AS avg_rating
+       ROUND(AVG(rating), 3) AS avg_rating,
+       COUNT(*)              AS total_movies
 FROM movies
 WHERE director IS NOT NULL
   AND director != ''
   AND rating IS NOT NULL
 GROUP BY director
-ORDER BY avg_rating DESC
+HAVING total_movies >= 5
+ORDER BY avg_rating DESC;
+
+-- How many movies are in each genre?
+SELECT movies.genres, COUNT(*) total_movies
+FROM movies
+WHERE genres IS NOT NULL
+GROUP BY genres
+ORDER BY total_movies DESC;
